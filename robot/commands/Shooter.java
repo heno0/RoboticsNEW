ea59b542006b50f96c2f70196614162d61646a7e
heno0 <96019650+heno0@users.noreply.github.com>
2022-01-30 10:54:31 -0700

    Update Shooter.java

--- robot/commands/Shooter.java
+++ robot/commands/Shooter.java
@@ -2,6 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+
+// DO NOT USE THIS COMMAND! TAKEN OUT OF USE IN v2.3 AND ONWARDS
+
+
 package frc.robot.commands;
 
 import com.revrobotics.CANSparkMax;
