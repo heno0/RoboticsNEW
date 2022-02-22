9d504ac3ada488abe34aec4cdf59982deecfe64f
heno0 <96019650+heno0@users.noreply.github.com>
2022-02-22 16:37:30 -0700

    v6.000000000001

--- robot/commands/IntakeCommand.java
+++ robot/commands/IntakeCommand.java
@@ -2,20 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
-import frc.robot.subsystems.Intake;
 
-public class IntakeCommand extends CommandBase {
-  private boolean activate;
-  
+
+
+// BAD BAD BAD BAD BAD BAD DO NOT USE BAD 
+
+
+
+
+
+
   private Compressor compressor;
   private DoubleSolenoid sol2;
   private Intake intakeC;
